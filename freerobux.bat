d31b3a446e8c95648dd77baa96f7f7a9e1dd3e56
lolvirus942 <minhquan123lo123@gmail.com>
2024-10-26 19:18:48 +0700

    Don't open

---
+++ freerobux.bat
@@ -0,0 +1,5 @@
+:: NOTE - EDUCATIONAL PURPOSES ONLY !!
+@echo off
+set IMG_URL=https://jollycontrarian.com/images/6/6c/Rickroll.jpg
+powershell -Command "Invoke-WebRequest -Uri '%IMG_URL%' -OutFile '%TEMP%\wallpaper.jpg'"
+powershell -Command "Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name Wallpaper -Value '%TEMP%\wallpaper.jpg'; Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class Wallpaper { [DllImport(\"user32.dll\")] public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni); }'; [Wallpaper]::SystemParametersInfo(20, 0, '%TEMP%\wallpaper.jpg', 3)"
